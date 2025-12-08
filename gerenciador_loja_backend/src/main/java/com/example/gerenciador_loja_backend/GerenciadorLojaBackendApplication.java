34a9a9d6a09a2fdcef7606d377162cdc00944792
Miguel Lopes <mpmiguel2007@gmail.com>
2025-12-08 14:26:22 -0300

    first commit

---
+++ gerenciador_loja_backend/src/main/java/com/example/gerenciador_loja_backend/GerenciadorLojaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerenciador_loja_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorLojaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorLojaBackendApplication.class, args);
+	}
+
+}
